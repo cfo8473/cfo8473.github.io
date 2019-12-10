4ffe4519f6d34e3a343346d3155a4d3b112b045c
CF <cfong7411@gmail.com>
2019-12-10 11:15:54 -0800

    Style fixes

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,21 +1,28 @@
+//Only Use the IntersectionObserver if it is supported
+if (IntersectionObserver) {
+  //When the element is visible on the viewport, 
+  //add the animated class so it creates the animation.
+  let callback = function (entries) {
+    entries.forEach(entry => {
+      //if the element is visible, add the animated class
+      if (entry.isIntersecting &&
+        !entry.target.classList.contains('animated')) {
+        entry.target.classList.add('animated');
+      } else { 
+        entry.target.classList.remove('animated');
+      }
+    });
+  }
+  //Create the observer
+  let observer = new IntersectionObserver(callback, {
+    root: null,
+    threshold: 0.3
+  });
 
-var intersectionOptions = {
-  root: null,  // use the viewport
-  rootMargin: '0px',
-  threshold: 0.3
-}
-
-function intersectionCallback(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio >= 1) {
-      console.log("Fully visible!");
-    } else {
-      console.log("Not fully visible!");
-    }
+  //Get and observe all the items with the item class
+  let items = document.querySelectorAll('.col-3');
+  items.forEach((item) => {
+    
+    observer.observe(item);
   });
 }
-
-var observer = new IntersectionObserver(intersectionCallback, intersectionOptions);
-
-var target = document.querySelector('#features');
-observer.observe(target);
